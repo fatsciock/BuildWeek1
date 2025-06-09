2590d1c8dadc4ebb53b2879efc69a3fa2b01de78
Andrea Negri <andrea.negri4@studio.unibo.it>
2025-06-09 20:54:49 +0200

    Creato PlayerController

---
+++ Assets/_Project/Scripts/LifeController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeController : MonoBehaviour
+{
+    public enum ON_DEFEAT_BEHAVIOUR { DISABLE = 0, DESTROY = 1, NONE = 2 }
+
+    [SerializeField] private int _currentHp = 10;
+    [SerializeField] private int _maxHp = 10;
+    [SerializeField] private bool _fullHpOnStart = true;
+    [SerializeField] private ON_DEFEAT_BEHAVIOUR _onDefeatBehaviour = ON_DEFEAT_BEHAVIOUR.DISABLE;
+
+    public int GetHp() => _currentHp;
+    public int GetMaxHp() => _maxHp;
+
+    // int valore = life.GetHp() + 5;
+    // life.SetHp( life.GetHp() + 5 );
+    // life.AddHp( 5 );
+
+    public void AddHp(int amount) => SetHp(_currentHp + amount);
+
+    public void SetHp(int hp)
+    {
+        hp = Mathf.Clamp(hp, 0, _maxHp);
+
+        if (_currentHp != hp)
+        {
+            _currentHp = hp;
+
+            if (_currentHp == 0)
+            {
+                Debug.Log($"The {gameObject.name} GameObject has been defeated!");
+                switch (_onDefeatBehaviour)
+                {
+                    default:
+                        Debug.LogError($"The option {_onDefeatBehaviour} is not recognized!");
+                        break;
+                    case ON_DEFEAT_BEHAVIOUR.DISABLE:
+                        gameObject.SetActive(false);
+                        break;
+                    case ON_DEFEAT_BEHAVIOUR.DESTROY:
+                        Destroy(gameObject);
+                        break;
+                    case ON_DEFEAT_BEHAVIOUR.NONE:
+                        break;
+                }
+            }
+        }
+    }
+
+    public void SetMaxHp(int maxHp)
+    {
+        _maxHp = Mathf.Max(1, maxHp);
+        SetHp(_currentHp);
+    }
+
+    private void Start()
+    {
+        if (_fullHpOnStart)
+        {
+            SetHp(_maxHp);
+        }
+    }
+}
--- Assets/_Project/Scripts/PlayerController.cs
+++ Assets/_Project/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private TopDownMover2D _mover;
+
     void Start()
     {
-        
+        _mover = GetComponent<TopDownMover2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 dir = new Vector2(h, v);
+        _mover.UpdateDirection(dir);
     }
 }
